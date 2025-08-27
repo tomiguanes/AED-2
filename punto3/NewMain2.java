d201fd3753470539a9906a1630cadadc66b2cde2
tomiguanes <tomasguanesce@gmail.com>
2025-08-26 21:21:59 -0300

    punto 3 tp 7 strings

---
+++ punto3/NewMain2.java
@@ -0,0 +1,31 @@
+/*
+3. Subcadenas y reemplazos
+Ingresar un correo electrónico:
+• Extraer el nombre de usuario (parte antes de @)
+• Reemplazar el dominio por "instituto.edu"
+Ingresar una frase y:
+• Dividirla en palabras
+• Mostrar cuántas palabras tiene.
+ */
+
+import java.util.Scanner;
+
+public class NewMain2 {
+
+    public static void main(String[] args) {
+    //creacion scanner
+    Scanner input=new Scanner(System.in);
+    
+        System.out.println("Ingrese mail: ");
+        String mailInput = input.nextLine().trim();
+      
+    
+    
+    
+    
+    }
+       
+    
+    
+    
+}
