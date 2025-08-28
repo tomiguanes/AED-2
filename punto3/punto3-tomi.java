2c3d59403dcf102e5b30d48adbfd813d8ed43546
tomiguanes <tomasguanesce@gmail.com>
2025-08-28 20:42:47 -0300

    Update punto3-tomi.java

--- punto3/punto3-tomi.java
+++ punto3/punto3-tomi.java
@@ -10,17 +10,24 @@ Ingresar una frase y:
 
 import java.util.Scanner;
 
-public class NewMain2 {
+public class NewMain1 {
 
     public static void main(String[] args) {
     //creacion scanner
-    Scanner input=new Scanner(System.in);
+    Scanner input=new Scanner(System.in);           //declaracion del objeto Scanner
     
-        System.out.println("Ingrese mail: ");
-        String mailInput = input.nextLine().trim();
-      
-    
-    
+        System.out.println("Ingrese mail: ");       
+        String mailInput = input.nextLine().trim(); //cargamos el input sin espacios a los extremos(trim) en la clase String
+        //aca faltarian las validaciones correspondientes
+        
+        int at = mailInput.indexOf('@');            //cargamos el indice del arroba en variable at
+        String usuario = mailInput.substring(0, at);   //desde 0 al arroba cargamos el usuario 
+        String dominio = mailInput.substring(at + 1);  //desde el caracter siguiente al arroba cargamos el dominio
+        String mailModificado = mailInput.replaceAll(dominio, "instituto.edu");
+        System.out.println("Mail ingresado: "+(mailInput));
+        System.out.println("Usuario: "+(usuario));
+        System.out.println("Dominio: "+(dominio));
+        System.out.println("Mail modificado: "+(mailModificado));
     
     
     }
@@ -29,3 +36,4 @@ public class NewMain2 {
     
     
 }
+
