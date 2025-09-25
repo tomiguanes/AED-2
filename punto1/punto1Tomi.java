e5ca91c21305b8dd555c43da2ca39a3146c7a76c
tomiguanes <tomasguanesce@gmail.com>
2025-09-25 20:19:03 -0300

    Update punto1Tomi.java

--- punto1/punto1Tomi.java
+++ punto1/punto1Tomi.java
@@ -1,29 +1,25 @@
 
-//----PUNTO UNO TP 7----
-import java.util.Scanner;
+public static void operacionesBasicas(Scanner input){
 
-public class NewMain {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        //creacion scanner
-    Scanner input=new Scanner(System.in);
     //solicitar nombre
+    String nameInput;   
+    do {
         System.out.print("Ingresar nombre: ");
-    String nameInput = input.nextLine();
-        String name = nameInput.trim();
-        String nameOp= name.replaceAll("\\s+", "");
+        nameInput = input.nextLine().trim();
+        if (nameInput.isEmpty()){
+            System.out.println("El nombre no puede estar vacio.");
+        }
+    } while (nameInput.isEmpty());
+        String name= nameInput.replaceAll("\\s+", "");
         System.out.println("Hola "+name);    
         //mostrar cantidad de caracteres
-        int cantChar = nameOp.length();
+        int cantChar = name.length();
         System.out.println("Tu nombre tiene "+cantChar+" letras.");
         //mostrar la ultima letra
-        char last = nameOp.charAt(cantChar-1);
+        char last = name.charAt(cantChar-1);
         System.out.println("Ultimo caracter: "+last);
         //mostrar la primera letra
-        char first = nameOp.charAt(0);
+        char first = name.charAt(0);
         System.out.println("Primer caracter: "+first);
         //convertir todo a mayuscula
         String nameMayus = name.toUpperCase();
@@ -32,5 +28,3 @@ public class NewMain {
         String nameMinus = name.toLowerCase();
         System.out.println("Nombre en minuscula: "+nameMinus);
     }
-    
-}
